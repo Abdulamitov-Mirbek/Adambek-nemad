38e164aef16c7599f9bfb9bac5d2c4fa83059d44
akiosmonov <darken452@gmail.com>
2026-04-04 17:03:35 +0600

    fead: update vite config for docker server

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    watch: {
+      usePolling: true, // Включаем опрос файлов
+    },
+    host: true,
     port: 3000,
-    open: true
-  }
+  },
 })
