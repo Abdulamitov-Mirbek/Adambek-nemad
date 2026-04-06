cbef689020b5bf248a39d5381c66146462c9b3b3
akiosmonov <darken452@gmail.com>
2026-04-06 14:13:27 +0600

    fead: update vite config and plugin

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,35 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import obfuscator from 'rollup-plugin-javascript-obfuscator'
 
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     watch: {
-      usePolling: true, // Включаем опрос файлов
+      usePolling: true,
     },
     host: true,
     port: 3000,
+  },
+
+  build: {
+    sourcemap: false, // Обязательно отключаем карты кода
+    minify: 'terser',
+    rollupOptions: {
+      plugins: [
+        obfuscator({
+          compact: true,
+          controlFlowFlattening: true,
+          deadCodeInjection: true,
+          debugProtection: true, // Защита от открытия DevTools
+          debugProtectionInterval: 2000,
+          disableConsoleOutput: true, // Отключает console.log в билде
+          identifierNamesGenerator: 'hexadecimal',
+          stringArray: true,
+          stringArrayThreshold: 0.75,
+        }),
+      ],
+    },
   },
 })
